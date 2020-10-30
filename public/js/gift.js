73fcad465829fb16d5e171e7c8177d12a3a6b8a4
niemet0502 <vincentmarius8@gmail.com>
2020-10-30 05:29:59 +0100

    added new card

--- public/js/gift.js
+++ public/js/gift.js
@@ -11,31 +11,56 @@ const fetchGift = async () => {
   query.find().then(function(gifts){
       if(gifts){
         var div = document.querySelector('.gifts');
-        div.setAttribute('class', 'container-fluid retailer-list');
-        var row = document.createElement('div');
-        row.setAttribute('class', 'row');
+      
         gifts.forEach(gift => {
           //je log les objects. 
           // pour recuperer un atribut, tu fait gift.get("nom_attribut")
           // 
           var col = document.createElement('div');
-          col.setAttribute('class', 'col-sm-6');
+          col.setAttribute('class', 'col-sm-4');
           var content = document.createElement('div');
-          content.setAttribute('class', 'retailer-thumbnail d-flex align-items-content justify-content-center');
+          content.setAttribute('class', 'block');
+          var top = document.createElement('div');
+          top.setAttribute('class', 'top'); 
+          var span = document.createElement('span');
+          span.setAttribute('class', 'converse');
+          span.innerHTML = gift.get("source");
+          top.appendChild(span);
+          
+          var middle = document.createElement('div');
+          middle.setAttribute('class', 'middle');
+
           var img = document.createElement('img');
           img.setAttribute('src', 'img/placeholders/360x120.png');
-          var title =  document.createElement('h4');
-          title.setAttribute('class', 'text-center');
-          title.innerHTML = gift.get("name");
-          content.appendChild(img);
+
+          middle.appendChild(img);
+
+          content.appendChild(top);
+          content.appendChild(middle);
+    
+          var bottom = document.createElement('div');
+          bottom.setAttribute('class', 'bottom');
+          var heading = document.createElement('div');
+          heading.setAttribute('class', 'heading');
+          heading.innerHTML = gift.get("name");
+          var quantity = document.createElement('div');
+          quantity.setAttribute('class', 'style');
+          quantity.innerHTML = "Quantité: "+gift.get("quantity");
+
+          var price = document.createElement('div');
+          price.setAttribute('class', 'price');
+          price.innerHTML = gift.get("price")+" FCFA";
+
+          bottom.appendChild(heading);
+          bottom.appendChild(quantity);
+          bottom.appendChild(price);
+
+          content.appendChild(bottom);
           col.appendChild(content);
-          col.appendChild(title);
-
-          row.appendChild(col);
-
-          console.log(gift.get("name"));
+          div.appendChild(col);
+          console.log(gift.get());
         });
-        div.appendChild(row);
+       
       } else {
           console.log("Nothing found, please try again");
       }
